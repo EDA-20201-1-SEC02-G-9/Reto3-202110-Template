cc413930fdec5b46ea8e5e79e7c0e15fd743c4f3
Diego <dramirezgarrido@hotmail.com>
2021-04-20 16:31:32 -0500

    Catalogo MK1

--- App/model.py
+++ App/model.py
@@ -39,6 +39,50 @@ los mismos.
 
 # Construccion de modelos
 
+class file_proc:
+    def __init__(self, filepath: str, *args):
+        self.filepath = filepath
+        self.index_types = lt.newList(datastructure='ARRAY_LIST')
+        for itype in args:
+            lt.addLast(self.index_types, itype)
+
+class catalog:
+    def array_line(self, line, index_types):
+        if line:
+            array = lt.newList(datastructure='ARRAY_LIST')
+            i = 1
+            for element in line.items():
+                element_obj = element[1].replace('"', '')
+                if lt.getElement(index_types, i) == 'f':
+                    lt.addLast(categories, float(element_obj))
+                elif lt.getElement(index_types, i) == 'i':
+                    lt.addLast(categories, int(element_obj))
+                else:
+                    lt.addLast(categories, element_obj)
+                i += 1
+            return array
+        else:
+            return None
+
+    def create_matrix(self, file: file_proc):
+        filepath = file.filepath
+        index_types = file.index_types
+        if filepath is not None:
+            input_file = csv.DictReader(open(filepath, encoding="utf-8"), delimiter=',')
+            matrix = lt.newList(datastructure='ARRAY_LIST')
+            for line in input_file:
+                array = self.array_line(line, index_types)
+                if array:
+                    lt.addLast(matrix, array)
+            return matrix
+
+    def __init__(self, file_basic: file_proc, file_characteristics: file_proc, file_sentiments: file_proc):
+        self.basic_catalog = self.create_matrix(file_basic)
+        self.characteristics_catalog = self.create_matrix(file_characteristics)
+        self.sentiments_catalog = self.create_matrix(file_sentiments)
+
+
+
 # Funciones para agregar informacion al catalogo
 
 # Funciones para creacion de datos
